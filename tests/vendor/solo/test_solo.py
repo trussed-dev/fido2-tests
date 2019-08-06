0f7e3aaf9702eba57d0e45efe95166638d23e9d0
Conor Patrick <conorpp94@gmail.com>
2019-08-06 18:36:46 +0800

    vendor/solo tests

---
+++ tests/vendor/solo/test_solo.py
@@ -0,0 +1,95 @@
+import math
+from binascii import hexlify
+
+import pytest
+
+from solo.client import SoloClient
+from solo.commands import SoloExtension
+
+from fido2.ctap1 import ApduError
+from fido2.utils import sha256
+
+from fido2.hid import CTAPHID
+from fido2.ctap import CtapError
+
+def shannon_entropy(data):
+    s = 0.0
+    total = len(data)
+    for x in range(0, 256):
+        freq = data.count(x)
+        p = freq / total
+        if p > 0:
+            s -= p * math.log2(p)
+    return s
+
+@pytest.fixture(scope="module", params = ['u2f'])
+def solo(request,device):
+        sc = SoloClient()
+        sc.find_device(device.dev)
+        if request.param == 'u2f':
+            sc.use_u2f()
+        else:
+            sc.use_hid()
+        return sc
+
+class TestSolo(object):
+    def test_solo(self,solo):
+        pass
+
+    def test_rng(self,solo):
+
+        total = 1024 * 16
+        entropy = b""
+        while len(entropy) < total:
+            entropy += solo.get_rng()
+
+        s = shannon_entropy(entropy)
+        assert s > 7.98
+        print("Entropy is %.5f bits per byte." % s)
+
+    def test_version(self,solo):
+        assert len(solo.solo_version()) == 3
+
+    def test_bootloader_not(self,solo):
+        with pytest.raises(ApduError) as e:
+            solo.write_flash(0x0, b"1234")
+
+    def test_fido2_bridge(self,solo):
+        exchange = solo.exchange
+        solo.exchange = solo.exchange_fido2
+
+        req = SoloClient.format_request(SoloExtension.version, 0, b"A" * 16)
+        a = solo.ctap2.get_assertion(
+            solo.host, b"B" * 32, [{"id": req, "type": "public-key"}]
+        )
+
+        assert a.auth_data.rp_id_hash == sha256(solo.host.encode("utf8"))
+        assert a.credential["id"] == req
+        assert (a.auth_data.flags & 0x5) == 0x5
+
+        assert len(solo.solo_version()) == 3
+        solo.get_rng()
+
+        solo.exchange = exchange
+
+
+    #def test_bootloader(self,):
+        #solo = SoloClient()
+        #solo.find_device(self.dev)
+        #solo.use_u2f()
+
+        #memmap = (0x08005000, 0x08005000 + 198 * 1024 - 8)
+        #data = b"A" * 64
+
+        #with Test("Test version command"):
+            #assert len(solo.bootloader_version()) == 3
+
+        #with Test("Test write command"):
+            #solo.write_flash(memmap[0], data)
+
+        #for addr in (memmap[0] - 8, memmap[0] - 4, memmap[1], memmap[1] - 8):
+            #with Test("Test out of bounds write command at 0x%04x" % addr):
+                #try:
+                    #solo.write_flash(addr, data)
+                #except CtapError as e:
+                    #assert e.code == CtapError.ERR.NOT_ALLOWED
