7fe35294795dad768f82514a5f539596d932ee5a
Conor Patrick <conorpp94@gmail.com>
2019-10-08 11:23:57 -0400

    add tests for backup key

--- tests/vendor/solo/test_solo.py
+++ tests/vendor/solo/test_solo.py
@@ -2,11 +2,12 @@ import sys
 
 import pytest
 from fido2.ctap1 import ApduError
+from fido2.ctap2 import CtapError
 from fido2.utils import sha256
 from solo.client import SoloClient
 from solo.commands import SoloExtension
 
-from tests.utils import shannon_entropy
+from tests.utils import shannon_entropy, verify, FidoRequest
 
 
 @pytest.fixture(scope="module", params=["u2f"])
@@ -19,9 +20,11 @@ def solo(request, device):
         sc.use_hid()
     return sc
 
+IS_EXPERIMENTAL = '--experimental' in sys.argv
+IS_NFC = '--nfc' in sys.argv
 
 @pytest.mark.skipif(
-    '--nfc' in sys.argv,
+    IS_NFC,
     reason="Wrong transport"
 )
 class TestSolo(object):
@@ -70,23 +73,52 @@ class TestSolo(object):
 
         solo.exchange = exchange
 
-    # def test_bootloader(self,):
-    # solo = SoloClient()
-    # solo.find_device(self.dev)
-    # solo.use_u2f()
+    @pytest.mark.skipif(not IS_EXPERIMENTAL, reason="Experimental")
+    def test_load_external_key_wrong_length(self,solo, ):
+        ext_key_cmd = 0x62
+        with pytest.raises(CtapError) as e:
+            solo.send_data_hid(ext_key_cmd, b'\x01\x00\x00\x00' + b'wrong length'*2)
+        assert(e.value.code == CtapError.ERR.INVALID_LENGTH)
 
-    # memmap = (0x08005000, 0x08005000 + 198 * 1024 - 8)
-    # data = b"A" * 64
+    @pytest.mark.skipif(not IS_EXPERIMENTAL, reason="Experimental")
+    def test_load_external_key_invalidate_old_cred(self,solo, device, MCRes, GARes):
+        ext_key_cmd = 0x62
+        verify(MCRes, GARes)
+        solo.send_data_hid(ext_key_cmd, b'\x01\x00\x00\x00' + b'Z' * 96)
 
-    # with Test("Test version command"):
-    # assert len(solo.bootloader_version()) == 3
+        # Old credential should not exist now.
+        with pytest.raises(CtapError) as e:
+            ga_bad_req = FidoRequest(GARes)
+            device.sendGA(*ga_bad_req.toGA())
+        assert(e.value.code == CtapError.ERR.NO_CREDENTIALS)
 
-    # with Test("Test write command"):
-    # solo.write_flash(memmap[0], data)
 
-    # for addr in (memmap[0] - 8, memmap[0] - 4, memmap[1], memmap[1] - 8):
-    # with Test("Test out of bounds write command at 0x%04x" % addr):
-    # try:
-    # solo.write_flash(addr, data)
-    # except CtapError as e:
-    # assert e.code == CtapError.ERR.NOT_ALLOWED
+    @pytest.mark.skipif(not IS_EXPERIMENTAL, reason="Experimental")
+    def test_load_external_key(self,solo, device,):
+        
+        key_A = b'A' * 96
+        key_B = b'B' * 96
+        ext_key_cmd = 0x62
+        solo.send_data_hid(ext_key_cmd, b'\x01\x00\x00\x00' + key_A)
+
+        # New credential works.
+        mc_A_req = FidoRequest()
+        mc_A_res = device.sendMC(*mc_A_req.toMC())
+
+        allow_list = [{"id":mc_A_res.auth_data.credential_data.credential_id, "type":"public-key"}]
+        ga_A_req = FidoRequest(mc_A_req, allow_list=allow_list)
+        ga_A_res = device.sendGA(*FidoRequest(ga_A_req).toGA())
+
+        verify(mc_A_res, ga_A_res, ga_A_req.cdh)
+
+        # Load up Key B and verify cred A doesn't exist.
+        solo.send_data_hid(ext_key_cmd, b'\x01\x00\x00\x00' + key_B)
+        with pytest.raises(CtapError) as e:
+            ga_A_res = device.sendGA(*FidoRequest(ga_A_req).toGA())
+        assert(e.value.code == CtapError.ERR.NO_CREDENTIALS)
+
+        # Load up Key A and verify cred A is back.
+        solo.send_data_hid(ext_key_cmd, b'\x01\x00\x00\x00' + key_A)
+        ga_A_res = device.sendGA(*FidoRequest(ga_A_req).toGA())
+        verify(mc_A_res, ga_A_res, ga_A_req.cdh)
+
