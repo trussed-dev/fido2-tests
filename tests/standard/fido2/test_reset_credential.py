74b2d370d14c0b96de885230807ee82be3bb78db
Andrew Kozlik <andrew.kozlik@gmail.com>
2019-10-08 13:16:59 +0200

    Enable automated user-presence tests on Trezor.

--- tests/standard/fido2/test_reset_credential.py
+++ tests/standard/fido2/test_reset_credential.py
@@ -1,9 +1,11 @@
+import sys
 import pytest
 from fido2.ctap import CtapError
 
 from tests.utils import *
 
 
+@pytest.mark.skipif('trezor' in sys.argv, reason="Trezor does not invalidate server-resident credentials.")
 def test_credential_resets(device, MCRes, GARes):
     verify(MCRes, GARes)
     device.reset()
