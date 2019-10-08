9066a8d68094a2381548b325d15b06b32a2f93ee
Andrew Kozlik <andrew.kozlik@gmail.com>
2019-10-08 13:12:27 +0200

    Add resident key tests for devices which have a display and do not support get_next_assertion().

--- tests/standard/fido2/test_resident_key.py
+++ tests/standard/fido2/test_resident_key.py
@@ -6,7 +6,7 @@ from tests.utils import *
 
 
 @pytest.fixture(scope="module", params=["", "123456"])
-def SetPINRes(request, device):
+def SetPINRes(request, device, info):
 
     device.reset()
 
@@ -14,11 +14,12 @@ def SetPINRes(request, device):
     req = FidoRequest()
 
     if pin:
-        device.client.pin_protocol.set_pin(pin)
-        pin_token = device.client.pin_protocol.get_pin_token(pin)
-        pin_auth = hmac_sha256(pin_token, req.cdh)[:16]
+        if "clientPin" in info.options:
+            device.client.pin_protocol.set_pin(pin)
+            pin_token = device.client.pin_protocol.get_pin_token(pin)
+            pin_auth = hmac_sha256(pin_token, req.cdh)[:16]
 
-        req = FidoRequest(req, pin_protocol=1, pin_auth=pin_auth)
+            req = FidoRequest(req, pin_protocol=1, pin_auth=pin_auth)
 
     res = device.sendMC(*req.toMC())
     setattr(res, "request", req)
@@ -52,7 +53,8 @@ class TestResidentKey(object):
         if not MC_RK_Res.request.pin_protocol:
             assert "id" in GA_RK_Res.user.keys() and len(GA_RK_Res.user.keys()) == 1
 
-    def test_multiple_rk(self, device, MC_RK_Res):
+    @pytest.mark.skipif('trezor' in sys.argv, reason="Trezor does not support get_next_assertion() because it has a display.")
+    def test_multiple_rk_nodisplay(self, device, MC_RK_Res):
         auths = []
         regs = [MC_RK_Res]
         for i in range(0, 3):
@@ -82,8 +84,30 @@ class TestResidentKey(object):
         for x, y in zip(regs, auths):
             verify(x, y, req.cdh)
 
+    @pytest.mark.skipif('trezor' not in sys.argv, reason="Only Trezor has a display.")
+    def test_multiple_rk_display(self, device, MC_RK_Res):
+        regs = [MC_RK_Res]
+        for i in range(0, 3):
+            req = FidoRequest(MC_RK_Res, user=generate_user())
+            res = device.sendMC(*req.toMC())
+            setattr(res, "request", req)
+            regs.append(res)
+
+        for i, reg in enumerate(reversed(regs)):
+            req = FidoRequest(MC_RK_Res, options=None, on_keepalive=DeviceSelectCredential(i + 1))
+            res = device.sendGA(*req.toGA())
+            assert res.number_of_credentials is None
+
+            with pytest.raises(CtapError) as e:
+                device.ctap2.get_next_assertion()
+            assert e.value.code == CtapError.ERR.NOT_ALLOWED
+
+            assert res.user["id"] == reg.request.user["id"]
+            verify(reg, res, req.cdh)
+
+    @pytest.mark.skipif('trezor' in sys.argv, reason="Trezor does not support get_next_assertion() because it has a display.")
     @pytest.mark.skipif('solokeys' in sys.argv, reason="Initial SoloKeys model truncates displayName")
-    def test_rk_maximum_size(self, device, MC_RK_Res):
+    def test_rk_maximum_size_nodisplay(self, device, MC_RK_Res):
         """
         Check the lengths of the fields according to the FIDO2 spec
         https://github.com/solokeys/solo/issues/158#issue-426613303
@@ -108,8 +132,24 @@ class TestResidentKey(object):
             for y in ("name", "icon", "displayName", "id"):
                 assert user_max_GA.user[y] == user_max[y]
 
+    @pytest.mark.skipif('trezor' not in sys.argv, reason="Only Trezor has a display.")
+    def test_rk_maximum_size_display(self, device, MC_RK_Res):
+        """
+        Check the lengths of the fields according to the FIDO2 spec
+        https://github.com/solokeys/solo/issues/158#issue-426613303
+        https://www.w3.org/TR/webauthn/#dom-publickeycredentialuserentity-displayname
+        """
+        user_max = generate_user_maximum()
+        req = FidoRequest(MC_RK_Res, user=user_max)
+        resMC = device.sendMC(*req.toMC())
+        req = FidoRequest(MC_RK_Res, options=None)
+        resGA = device.sendGA(*req.toGA())
+        assert resGA.number_of_credentials is None
+        verify(resMC, resGA, req.cdh)
+
+    @pytest.mark.skipif('trezor' in sys.argv, reason="Trezor does not support get_next_assertion() because it has a display.")
     @pytest.mark.skipif('solokeys' in sys.argv, reason="Initial SoloKeys model truncates displayName")
-    def test_rk_maximum_list_capacity_per_rp(self, device, MC_RK_Res):
+    def test_rk_maximum_list_capacity_per_rp_nodisplay(self, device, MC_RK_Res):
         """
         Test maximum returned capacity of the RK for the given RP
         """
@@ -159,3 +199,32 @@ class TestResidentKey(object):
         assert len(auths) == len(regs)
         for x, y in zip(regs, auths):
             verify(x, y, req.cdh)
+
+    @pytest.mark.skipif('trezor' not in sys.argv, reason="Only Trezor has a display.")
+    def test_rk_maximum_list_capacity_per_rp_display(self, device):
+        """
+        Test maximum capacity of resident keys.
+        """
+        RK_CAPACITY = 16
+        device.reset()
+        req = FidoRequest(options={"rk": True})
+
+        regs = []
+        for i in range(RK_CAPACITY):
+            req = FidoRequest(req, user=generate_user_maximum())
+            res = device.sendMC(*req.toMC())
+            setattr(res, "request", req)
+            regs.append(res)
+
+        req = FidoRequest(req, user=generate_user_maximum())
+        with pytest.raises(CtapError) as e:
+            res = device.sendMC(*req.toMC())
+        assert e.value.code == CtapError.ERR.KEY_STORE_FULL
+
+        for i, reg in enumerate(reversed(regs)):
+            if i not in (0, 1, 7, 14, 15):
+                continue
+            req = FidoRequest(req, options=None, on_keepalive=DeviceSelectCredential(i + 1))
+            res = device.sendGA(*req.toGA())
+            assert res.user["id"] == reg.request.user["id"]
+            verify(reg, res, req.cdh)
