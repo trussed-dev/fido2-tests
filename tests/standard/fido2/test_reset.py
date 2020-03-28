9e41cf111d51050c0b79af7a64ae0c5c3b8648bc
Conor Patrick <conorpp94@gmail.com>
2020-03-28 15:59:59 -0400

    test reset fails after powered for 10s

--- tests/standard/fido2/test_reset.py
+++ tests/standard/fido2/test_reset.py
@@ -1,3 +1,5 @@
+import time
+import pytest
 from fido2.ctap import CtapError
 
 import tests
@@ -5,3 +7,12 @@ import tests
 
 def test_reset(device):
     device.reset()
+
+def test_reset_after_10s_fails(device):
+    device.reboot()
+    time.sleep(10.1)
+    with pytest.raises(CtapError) as e:
+        device.ctap2.reset()
+    assert e.value.code == CtapError.ERR.NOT_ALLOWED
+    
+
